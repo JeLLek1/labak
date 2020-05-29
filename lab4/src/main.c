e09fed4af4586abc2d2c8caf93affbd94232a172
Jellek <jellek@wp.pl>
2020-05-29 19:49:57 +0200

    poprawka

--- lab4/src/main.c
+++ lab4/src/main.c
@@ -7,7 +7,7 @@ typedef struct{
 }Number;
 
 typedef struct{
-	double sisd, simd;
+	long sisd, simd;
 }timeMeasure;
 
 //dodawanie wektorów a+b sekwencyjnie
@@ -48,25 +48,30 @@ timeMeasure measureAdd(int n){
 	timeRun.sisd = 0;
 	timeRun.simd = 0;
 	//pomiar dodawania
+	//losowanie wartości
+	random_vec(&numberA);
+	random_vec(&numberB);
+	start = clock();
 	for(int i=0; i<10; i++){
 		for(int j=0; j<n; j++){
-			random_vec(&numberA);
-			random_vec(&numberB);
 			numberA1=numberA;
 			numberB1=numberB;
-			start = clock();
-			addSISD(&numberA, numberB);
-			end = clock();
-			timeRun.sisd+=(double)(end-start)/CLOCKS_PER_SEC;
-			start = clock();
-			addSIMD(&numberA1, numberB1);
-			end = clock();
-			timeRun.simd+=(double)(end-start)/CLOCKS_PER_SEC;
+			addSISD(&numberA1, numberB1);
 		}
 	}
-	timeRun.sisd/=10;
-	timeRun.simd/=10;
-
+	end = clock();
+	timeRun.sisd =(end-start);
+	start = clock();
+	for(int i=0; i<10; i++){
+		for(int j=0; j<n; j++){
+			numberA1=numberA;
+			numberB1=numberB;
+			addSIMD(&numberA1, numberB1);
+		}
+		
+	}
+	end = clock();
+	timeRun.simd =(end-start);
 	return timeRun;
 }
 
@@ -85,24 +90,31 @@ timeMeasure measureSub(int n){
 	timeRun.sisd = 0;
 	timeRun.simd = 0;
 	//pomiar dodawania
+	//losowanie wartości
+	random_vec(&numberA);
+	random_vec(&numberB);
+	start = clock();
 	for(int i=0; i<10; i++){
 		for(int j=0; j<n; j++){
-			random_vec(&numberA);
-			random_vec(&numberB);
 			numberA1=numberA;
 			numberB1=numberB;
-			start = clock();
-			subSISD(&numberA, numberB);
-			end = clock();
-			timeRun.sisd+=(double)(end-start)/CLOCKS_PER_SEC;
-			start = clock();
-			subSIMD(&numberA1, numberB1);
-			end = clock();
-			timeRun.simd+=(double)(end-start)/CLOCKS_PER_SEC;
+			subSISD(&numberA1, numberB1);
 		}
 	}
-	timeRun.sisd/=10;
-	timeRun.simd/=10;
+	end = clock();
+	timeRun.sisd =(end-start);
+	//losowanie wartości
+	start = clock();
+	for(int i=0; i<10; i++){
+		for(int j=0; j<n; j++){
+			numberA1=numberA;
+			numberB1=numberB;
+			subSIMD(&numberA1, numberB1);
+		}
+		
+	}
+	end = clock();
+	timeRun.simd =(end-start);
 
 	return timeRun;
 }
@@ -122,24 +134,30 @@ timeMeasure measureMul(int n){
 	timeRun.sisd = 0;
 	timeRun.simd = 0;
 	//pomiar dodawania
+	//losowanie wartości
+	random_vec(&numberA);
+	random_vec(&numberB);
+	start = clock();
 	for(int i=0; i<10; i++){
 		for(int j=0; j<n; j++){
-			random_vec(&numberA);
-			random_vec(&numberB);
 			numberA1=numberA;
 			numberB1=numberB;
-			start = clock();
-			mulSISD(&numberA, numberB);
-			end = clock();
-			timeRun.sisd+=(double)(end-start)/CLOCKS_PER_SEC;
-			start = clock();
-			mulSIMD(&numberA1, numberB1);
-			end = clock();
-			timeRun.simd+=(double)(end-start)/CLOCKS_PER_SEC;
+			mulSISD(&numberA1, numberB1);
 		}
 	}
-	timeRun.sisd/=10;
-	timeRun.simd/=10;
+	end = clock();
+	timeRun.sisd =(end-start);
+	start = clock();
+	for(int i=0; i<10; i++){
+		for(int j=0; j<n; j++){
+			numberA1=numberA;
+			numberB1=numberB;
+			mulSIMD(&numberA, numberB);
+		}
+		
+	}
+	end = clock();
+	timeRun.simd =(end-start);
 
 	return timeRun;
 }
@@ -160,24 +178,30 @@ timeMeasure measureDiv(int n){
 	timeRun.sisd = 0;
 	timeRun.simd = 0;
 	//pomiar dodawania
+	//losowanie wartości
+	random_vec(&numberA);
+	random_vec(&numberB);
+	start = clock();
 	for(int i=0; i<10; i++){
 		for(int j=0; j<n; j++){
-			random_vec(&numberA);
-			random_vec(&numberB);
 			numberA1=numberA;
 			numberB1=numberB;
-			start = clock();
 			divSISD(&numberA, numberB);
-			end = clock();
-			timeRun.sisd+=(double)(end-start)/CLOCKS_PER_SEC;
-			start = clock();
-			divSIMD(&numberA1, numberB1);
-			end = clock();
-			timeRun.simd+=(double)(end-start)/CLOCKS_PER_SEC;
 		}
 	}
-	timeRun.sisd/=10;
-	timeRun.simd/=10;
+	end = clock();
+	timeRun.sisd =(end-start);
+	start = clock();
+	for(int i=0; i<10; i++){
+		for(int j=0; j<n; j++){
+			numberA1=numberA;
+			numberB1=numberB;
+			divSIMD(&numberA, numberB);
+		}
+		
+	}
+	end = clock();
+	timeRun.simd =(end-start);
 
 	return timeRun;
 }
@@ -196,42 +220,44 @@ int main(){
 	
 	timeMeasure timeRun = measureAdd(512);
 	//wynik do pliku
-	fprintf(file, "Czas dodawania dla 2048 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas dodawania dla 2048 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 	timeRun = measureAdd(1024);
 	//wynik do pliku
-	fprintf(file, "Czas dodawania dla 4096 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas dodawania dla 4096 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 	timeRun = measureAdd(2048);
 	//wynik do pliku
-	fprintf(file, "Czas dodawania dla 8192 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas dodawania dla 8192 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 
 	timeRun = measureSub(512);
 	//wynik do pliku
-	fprintf(file, "Czas odejmowania dla 2048 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas odejmowania dla 2048 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 	timeRun = measureSub(1024);
 	//wynik do pliku
-	fprintf(file, "Czas odejmowania dla 4096 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas odejmowania dla 4096 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 	timeRun = measureSub(2048);
 	//wynik do pliku
-	fprintf(file, "Czas odejmowania dla 8192 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas odejmowania dla 8192 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 
 	timeRun = measureMul(512);
 	//wynik do pliku
-	fprintf(file, "Czas mnozenia dla 2048 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas mnozenia dla 2048 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 	timeRun = measureMul(1024);
 	//wynik do pliku
-	fprintf(file, "Czas mnozenia dla 4096 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas mnozenia dla 4096 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 	timeRun = measureMul(2048);
 	//wynik do pliku
-	fprintf(file, "Czas mnozenia dla 8192 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas mnozenia dla 8192 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 
 	timeRun = measureDiv(512);
 	//wynik do pliku
-	fprintf(file, "Czas dzielenia dla 2048 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas dzielenia dla 2048 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 	timeRun = measureDiv(1024);
 	//wynik do pliku
-	fprintf(file, "Czas dzielenia dla 4096 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas dzielenia dla 4096 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
 	timeRun = measureDiv(2048);
 	//wynik do pliku
-	fprintf(file, "Czas dzielenia dla 8192 liczb: \nSISD: %f \nSIMD: %f \n\n", timeRun.sisd, timeRun.simd);
+	fprintf(file, "Czas dzielenia dla 8192 liczb: \nSISD: %ld \nSIMD: %ld \n\n", timeRun.sisd, timeRun.simd);
+
+	fprintf(file, "CLOCKS_PER_SEC: %ld", CLOCKS_PER_SEC);
 	return 0;
 } 
